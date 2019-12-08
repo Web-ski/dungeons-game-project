82e7713495a41ca8de5784dc574f53a2c6e72550
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2019-12-08 21:57:28 +0100

    Add function for positioning  walls

--- scripts/game-board.js
+++ scripts/game-board.js
@@ -2,20 +2,47 @@ const createWalls = () => {
 	const gameBoard = document.getElementsByClassName('game-board')[0];
 
 	const wallsArr = [];
-	for (let i = 0; i < 32; i++) {
+	for (let i = 0; i < 33; i++) {
 		wallsArr.push({});
 	}
 
-	console.log(wallsArr);
-
-		const addWalls = (item, index) => {
-			console.log(index);
-			let addWall = document.createElement('div');
-			gameBoard.appendChild(addWall);
-			gameBoard.children[index + 1].setAttribute('class', 'stone-walls');
-		};
+	const addWalls = (item, index) => {
+		let addWall = document.createElement('div');
+		gameBoard.appendChild(addWall);
+		gameBoard.children[index + 1].setAttribute('class', 'stone-walls');
+	};
 
 	wallsArr.map(addWalls);
+
+	const wallBlocksArr = gameBoard.getElementsByClassName('stone-walls');
+
+	const addWallPositions = (item, index) => {
+		
+		if (index < 9) {
+
+			let nmbr = index * 50;
+			item.style.top = '0px';
+			item.style.left = nmbr + 'px';
+		} else if (index > 8 && index < 17) {
+	
+			let nmbr = (index - 8) * 50;
+			item.style.top = nmbr + 'px';
+			item.style.left = 8*50 + 'px';
+		} else if (index > 16 && index < 25) {
+		
+			let nmbr = (index - 8*2) * 50;
+			item.style.top = 8*50 + 'px';
+			item.style.left = nmbr + 'px';
+		} else if (index > 14 && index < 33) {
+	
+			let nmbr = (index - 8*3) * 50;
+			item.style.top = nmbr + 'px';
+			item.style.left = '0px';
+		}
+
+	}
+
+	[].forEach.call(wallBlocksArr, addWallPositions);
 
 };
 
