64831aa445f51b5f900eed973df10aab83541659
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2019-12-08 21:04:08 +0100

    Add function adding wall elems

--- scripts/game-board.js
+++ scripts/game-board.js
@@ -1,9 +1,22 @@
 const createWalls = () => {
+	const gameBoard = document.getElementsByClassName('game-board')[0];
 
-    const wall = {
-        
-    }
+	const wallsArr = [];
+	for (let i = 0; i < 32; i++) {
+		wallsArr.push({});
+	}
 
-}
+	console.log(wallsArr);
+
+		const addWalls = (item, index) => {
+			console.log(index);
+			let addWall = document.createElement('div');
+			gameBoard.appendChild(addWall);
+			gameBoard.children[index + 1].setAttribute('class', 'stone-walls');
+		};
+
+	wallsArr.map(addWalls);
+
+};
 
 createWalls();
