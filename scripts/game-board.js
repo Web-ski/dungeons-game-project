98a162bb96f40d682c7d9ac54c7360adb0a82922
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2019-12-09 15:09:52 +0100

    Add function adding rooms

--- scripts/game-board.js
+++ scripts/game-board.js
@@ -1,49 +1,92 @@
-const createWalls = () => {
-	const gameBoard = document.getElementsByClassName('game-board')[0];
+const createRooms = () => {
+	const game = document.getElementsByClassName('game')[0];
 
-	const wallsArr = [];
-	for (let i = 0; i < 33; i++) {
-		wallsArr.push({});
-	}
+	//list of rooms
 
-	const addWalls = (item, index) => {
-		let addWall = document.createElement('div');
-		gameBoard.appendChild(addWall);
-		gameBoard.children[index + 1].setAttribute('class', 'stone-walls');
+	const roomStart = {
+		doors: 1111
 	};
 
-	wallsArr.map(addWalls);
+	const roomLeft0001 = {
+		doors: 0001
+	};
 
-	const wallBlocksArr = gameBoard.getElementsByClassName('stone-walls');
+	const roomRight0001 = {
+		doors: 0100
+	};
 
-	const addWallPositions = (item, index) => {
-		
-		if (index < 9) {
+	const roomTop0001 = {
+		doors: 1000
+	};
 
-			let nmbr = index * 50;
-			item.style.top = '0px';
-			item.style.left = nmbr + 'px';
-		} else if (index > 8 && index < 17) {
-	
-			let nmbr = (index - 8) * 50;
-			item.style.top = nmbr + 'px';
-			item.style.left = 8*50 + 'px';
-		} else if (index > 16 && index < 25) {
-		
-			let nmbr = (index - 8*2) * 50;
-			item.style.top = 8*50 + 'px';
-			item.style.left = nmbr + 'px';
-		} else if (index > 14 && index < 33) {
-	
-			let nmbr = (index - 8*3) * 50;
-			item.style.top = nmbr + 'px';
-			item.style.left = '0px';
+	const roomBottom0001 = {
+		doors: 0010
+	};
+
+	const roomsArr = [roomStart, roomTop0001, roomRight0001, roomBottom0001, roomLeft0001];
+
+	const createWalls = (item, index) => {
+		const addGameBoard = document.createElement('section');
+		const addGameField = document.createElement('div');
+		game.appendChild(addGameBoard);
+		game.children[index].setAttribute('class', 'game-board');
+		const gameBoard = game.getElementsByClassName('game-board')[index];
+		gameBoard.appendChild(addGameField);
+		gameBoard.firstChild.setAttribute('class', 'field');
+
+		const wallsArr = [];
+		for (let i = 0; i < 33; i++) {
+			wallsArr.push({});
 		}
 
-	}
+		const addWalls = (item, index) => {
+			let addWall = document.createElement('div');
+			gameBoard.appendChild(addWall);
+			gameBoard.children[index + 1].setAttribute('class', 'stone-walls');
+		};
 
-	[].forEach.call(wallBlocksArr, addWallPositions);
+		wallsArr.map(addWalls);
+
+		const wallBlocksArr = gameBoard.getElementsByClassName('stone-walls');
+
+		const addWallPositions = (item, index) => {
+
+			if (index < 9) {
+
+				let nmbr = index * 50;
+				item.style.top = '0px';
+				item.style.left = nmbr + 'px';
+			} else if (index > 8 && index < 17) {
+
+				let nmbr = (index - 8) * 50;
+				item.style.top = nmbr + 'px';
+				item.style.left = 8 * 50 + 'px';
+			} else if (index > 16 && index < 25) {
+
+				let nmbr = (index - 8 * 2) * 50;
+				item.style.top = 8 * 50 + 'px';
+				item.style.left = nmbr + 'px';
+			} else if (index > 14 && index < 33) {
+
+				let nmbr = (index - 8 * 3) * 50;
+				item.style.top = nmbr + 'px';
+				item.style.left = '0px';
+			}
+		}
+
+		[].forEach.call(wallBlocksArr, addWallPositions);
+
+		const createDoors = () => {
+
+
+		};
+
+	};
+
+	//createWalls();
+
+	roomsArr.map(createWalls);
 
 };
 
-createWalls();
+createRooms();
