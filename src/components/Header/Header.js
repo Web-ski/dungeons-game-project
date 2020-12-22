cde476b65c4680cbd5fc3919c66ffeb4883eb8de
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2020-12-23 00:03:05 +0100

    Add styles to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,34 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 const Header = () => {
+  const Section = styled.section`
+    width: 100%;
+    height: 70px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: grey;
+  `;
+
+  const StyledLink = styled(NavLink)`
+    color: white;
+    font-weight: bold;
+    text-decoration: none;
+    padding: 10px;
+
+    &:hover {
+      color: black;
+    }
+  `;
+
   return (
-    <section className="header">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/game">Game</NavLink>
-      <NavLink to="/about">About</NavLink>
-    </section>
+    <Section className="header">
+      <StyledLink to="/">Home</StyledLink>
+      <StyledLink to="/game">Game</StyledLink>
+      <StyledLink to="/about">About</StyledLink>
+    </Section>
   );
 };
 
