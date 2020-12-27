77ac0833cfb5908f44376e7abf8c02b536a3d05b
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2020-12-27 17:59:34 +0100

    Add hero json data

---
+++ src/components/UI/Mobs/GameHero.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const GameHero = styled.div`
+  position: absolute;
+  background-color: blueviolet;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+export default GameHero;
