183f0f64863c7e7369bb4473398b1cbc3535f23e
Paweł <pawel.nieczuja@onet.eu>
2020-12-29 14:47:17 +0100

    Change GameHero

--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
-import { changePositionAction } from "../../api/player/action";
 import styled from "styled-components";
 import RoomTile from "./RoomTile";
 import GameHero from "../UI/Mobs/GameHero";
-import { isBrowser, playerMove } from '../../accessors/player';
 
 const Room = (props) => {
   const [room, setRoom] = useState();
-  const [posX, setPosX] = useState();
-  const [posY, setPosY] = useState();
 
   useEffect(() => {
     props.rooms !== undefined &&
@@ -19,21 +15,6 @@ const Room = (props) => {
         });
       });
   }, [props.rooms]);
-
-  useEffect(() => {
-    props.positionX !== undefined && setPosX(props.positionX);
-    props.positionY !== undefined && setPosY(props.positionY);
-  }, [props.positionX, props.positionY]);
-
-  //oneKeyDown use with Hook
-  useEffect(() => {
-    isBrowser() && window.addEventListener('keydown', (event) => {
-      //console.log(event.keyCode);
-      //playerMove(event.keyCode);
-      props.move("100", "100");
-      console.log(props.positionX, props.positionY);
-    });
-  });
 
   const RoomBoard = styled.section`
     position: relative;
@@ -58,7 +39,7 @@ const Room = (props) => {
   return (
     <RoomBoard className="room">
       {addChildren(room)}
-      <GameHero top={posX + "px"} left={posY + "px"} />
+      <GameHero />
     </RoomBoard>
   );
 };
@@ -67,16 +48,6 @@ const mapStateToProps = (state) => ({
   activeRoom: state.game.activeRoom,
   rooms: state.game.roomsCollection,
   hero: state.player.hero,
-  positionX: state.player.positionX,
-  positionY: state.player.positionY,
-  game1: state.game.hero !== undefined && state.game.hero[0].positionX,
-  //heroX: state.hero.positionX,
 });
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    move: (dataX, dataY) => dispatch(changePositionAction(dataX, dataY)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Room);
+export default connect(mapStateToProps)(Room);
--- src/components/UI/Mobs/GameHero.js
+++ src/components/UI/Mobs/GameHero.js
@@ -1,8 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { isBrowser, playerMove } from '../../../accessors/player';
+import { changePositionAction } from "../../../api/player/action";
+
 import styled, { css } from "styled-components";
 // const HERO = "/gameData/images/gnome.svg";
 const HERO = "/gameData/images/chemist-svgrepo-com.svg";
 
-const GameHero = styled.div`
+const GameHero = (props) => {
+  const [posX, setPosX] = useState();
+  const [posY, setPosY] = useState();
+
+  useEffect(() => {
+    props.positionX !== undefined && setPosX(props.positionX);
+    props.positionY !== undefined && setPosY(props.positionY);
+  }, [props.positionX, props.positionY]);
+
+  //oneKeyDown use with Hook
+  useEffect(() => {
+    isBrowser() && window.addEventListener('keydown', (event) => {
+      //console.log(event.keyCode);
+      //playerMove(event.keyCode);
+      props.move("100", "100");
+      console.log(props.positionX, props.positionY);
+    });
+  });
+
+  const Hero = styled.div`
   position: absolute;
   width: 50px;
   height: 50px;
@@ -15,4 +39,23 @@ const GameHero = styled.div`
   background-repeat: no-repeat;
 `;
 
-export default GameHero;
+  return (
+      <Hero top={posX + "px"} left={posY + "px"} />
+  );
+}
+
+const mapStateToProps = (state) => ({
+  hero: state.player.hero,
+  positionX: state.player.positionX,
+  positionY: state.player.positionY,
+  game1: state.game.hero !== undefined && state.game.hero[0].positionX,
+  //heroX: state.hero.positionX,
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    move: (dataX, dataY) => dispatch(changePositionAction(dataX, dataY)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GameHero);
