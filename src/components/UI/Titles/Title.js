277d7eaf96701e1b550c2d1f07d0beb270377172
Paweł <pawel.nieczuja@onet.eu>
2021-01-06 18:39:02 +0100

    Change colors

--- src/components/UI/Titles/Title.js
+++ src/components/UI/Titles/Title.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 import { GAME_NAME } from "../../../constants/constants";
 
 const PageTitle = styled.h1`
-  color: #F9D870;
-  font-size: 30px;
+  color: #91C35C;
+  font-size: 40px;
   margin: 30px 0;
 `;
 
