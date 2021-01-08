4ec9828f6c95a4aa0c8b2f30d4ae8a0da71cd137
Paweł <pawel.nieczuja@onet.eu>
2021-01-08 18:01:57 +0100

    Change colors

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,8 +1,8 @@
 import styled, { css } from "styled-components";
 
 const Button = styled.button`
-  background-color: #0D4A28;
-  color: #F9D870;
+  background-color: purple;
+  color: #65ECBB;
   padding: 15px 25px;
   margin: 20px;
   letter-spacing: 2px;
