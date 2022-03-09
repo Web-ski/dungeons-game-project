aaaa5bf198a41abe7ff96d0f53601a302301360f
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2022-03-09 22:39:11 +0100

    Add structure step 2

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/game">Game</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
