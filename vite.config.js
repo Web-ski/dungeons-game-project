158e59a7f8678b46085d02e29d508f91997c818a
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2022-03-31 13:32:45 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,14 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/dungeons-game-project/" // prod
+      : "./", // dev,
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: process.env.NODE_ENV,
 });
